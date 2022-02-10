2cadeeec8bed09fa19e9cb945fa9b51ded04debe
tsidevhp001 <tsidevhp001@TSI-HP-001>
2022-02-10 13:19:14 +0530

    Add project files.

---
+++ do while/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("i value: {0}", i);
+                i++;
+                int j = 1;
+                do
+                {
+                    Console.WriteLine("j value: {0}", j);
+                    j++;
+                } while (j < 2);
+            } while (i < 4);
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
